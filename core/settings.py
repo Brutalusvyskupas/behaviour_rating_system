54ac822cd62e9414265e0404661ab566982e631f
paulius <fixas1029@gmail.com>
2021-08-31 09:15:39 +0300

    changed media url to https

--- core/settings.py
+++ core/settings.py
@@ -177,7 +177,7 @@ AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_LOCATION = 'static'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = 'http://{!s}.s3.amazonaws.com/media/'.format(AWS_STORAGE_BUCKET_NAME) # IMPORTANT 
+MEDIA_URL = 'https://{!s}.s3.amazonaws.com/media/'.format(AWS_STORAGE_BUCKET_NAME) # IMPORTANT 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
